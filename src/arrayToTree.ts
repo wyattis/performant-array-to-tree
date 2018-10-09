9781cd0cc2e180f8231577ab225fdcd3a9301b9f
wyattis <wyattisrael@gmail.com>
2018-10-09 17:08:56 -0400

    Made the typescript actually work for generic types and different id and parentId keys passed in from the config.

--- src/arrayToTree.ts
+++ src/arrayToTree.ts
@@ -1,12 +1,6 @@
-export interface Item {
-  id: string
-  parentId: string|null,
-  [key: string]: any,
-}
-
-export interface TreeItem {
-  data: Item|null
-  children: TreeItem[]
+export interface TreeItem<T> {
+  data: T|null
+  children: TreeItem<T>[]
 }
 
 export interface Config {
@@ -17,25 +11,25 @@ export interface Config {
 /**
  * Unflattens an array to a tree with runtime O(n)
  */
-export function arrayToTree (items: Item[], config: Config = { id: 'id', parentId: 'parentId' }): TreeItem[] {
+export function arrayToTree<T> (items: T[], config: Config = { id: 'id', parentId: 'parentId' }): TreeItem<T>[] {
   // the resulting unflattened tree
-  const rootItems: TreeItem[] = []
+  const rootItems: TreeItem<T>[] = []
 
   // stores all already processed items with ther ids as key so we can easily look them up
-  const lookup: { [id: string]: TreeItem } = {}
+  const lookup: { [id: string]: TreeItem<T> } = {}
 
   // idea of this loop:
   // whenever an item has a parent, but the parent is not yet in the lookup object, we store a preliminary parent
   // in the lookup object and fill it with the data of the parent later
   // if an item has no parentId, add it as a root element to rootItems
   for (const item of items) {
-    const itemId = item[config.id]
-    const parentId = item[config.parentId]
+    const itemId = (item as any)[config.id]
+    const parentId = (item as any)[config.parentId]
 
     // look whether item already exists in the lookup table
     if (!Object.prototype.hasOwnProperty.call(lookup, itemId)) {
       // item is not yet there, so add a preliminary item (its data will be added later)
-      lookup[itemId] = { data: null, children: [] }
+      lookup[itemId] = { data: null, children: [] } as TreeItem<T>
     }
 
     // add the current item's data to the item in the lookup table
